501db2d02e43d75b35dba970bf3ca25840098e66
Reza Nikravesh <reza.nikravesh80@gmail.com>
2022-06-25 17:54:01 +0430

    create loader

---
+++ components/Loader.tsx
@@ -0,0 +1,11 @@
+import classes from "../scss/modules/Loader.module.scss";
+
+const Loader = () => {
+  return (
+    <div className={classes.container}>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from "next";
+import Loader from "../components/Loader";
 import Sidebar from "../components/Sidebar";
 
 const Home: NextPage = () => {
-  return <div>
-    <Sidebar/>
-  </div>;
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Home;
